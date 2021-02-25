5dd410efb1d27561210788ac2966d3e106740109
Rafael Couto <52931317+rafaelcouto-coder@users.noreply.github.com>
2021-02-25 19:29:01 -0300

    create service mail

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
+import SendMailService from "../services/SendMailService";
 
 class SendMailController {
 
@@ -21,9 +22,9 @@ class SendMailController {
             });
         }
 
-        const surveyAlreadyExists = await surveysRepository.findOne({id: survey_id});
+        const survey = await surveysRepository.findOne({id: survey_id});
 
-        if(!surveyAlreadyExists){
+        if(!survey){
             return response.status(400).json({
                 error: "Survey does not exists!",
             });
@@ -37,6 +38,8 @@ class SendMailController {
         })
         await surveysUsersRepository.save(surveyUser);
         // enviar e-mail para o usuário
+
+        await SendMailService.execute(email, survey.title, survey.description);
 
         return response.json(surveyUser);
     }
